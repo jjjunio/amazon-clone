f58e77d810d02700e1d74bb74d640ebfde4a1fbf
jjjunio <jjjunio@gmail.com>
2020-09-17 14:27:39 -0400

    creating Header, Home, and Product components

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import Header from "./Header";
+import Home from "./Home";
+
+
+function App() {
+  return (
+    //Use BEM always!
+    <div className="App">
+      <Header />
+      <Home />
+    </div>
+
+  );
+}
+
+export default App;
+
+
+
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAtOhbx1LY-GMiaIR1CqnWM4uy9HH6mats",
+  authDomain: "clone-a6f70.firebaseapp.com",
+  databaseURL: "https://clone-a6f70.firebaseio.com",
+  projectId: "clone-a6f70",
+  storageBucket: "clone-a6f70.appspot.com",
+  messagingSenderId: "202335487388",
+  appId: "1:202335487388:web:515ff570100777df803459",
+  measurementId: "G-SW9YNY3SE9"
+};
