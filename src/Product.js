6fb335458b5725c962dce616e2b58b646f309ab9
jjjunio <jjjunio@gmail.com>
2020-09-17 16:42:40 -0400

    creating Subtotal, Checkout components and adding stylesheets

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import "./Product.css";
+import { useStateValue } from "./StateProvider"
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log("this is the basket>>>>", basket);
+
+  const addToBasket = () => {
+    //dispatch item to the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    })
+  }
+
   return (
     <div className="product">
       <div className="product__info">
@@ -26,7 +45,7 @@ function Product({ id, title, image, price, rating }) {
         alt="The Lean Startup book cover"
       />
 
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
 
 
 
